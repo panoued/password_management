a6f9c2068dc03b6e8a6f39aa4b33c29037e21e52
panoued <panoued@163.com>
2020-02-29 03:00:36 +0800

    update

--- src/views/auth/Login.ts
+++ src/views/auth/Login.ts
@@ -1,14 +1,10 @@
 import { Component, Vue } from 'vue-property-decorator';
-import { isSupportFingerprint, isEnrolledFingerprints } from '@/utils/H5PlusUtil';
 
 @Component
 export default class Login extends Vue {
     
     mounted() {
-        setTimeout(() => {
-            console.log(isSupportFingerprint());
-            console.log(isEnrolledFingerprints());
-        }, 1000);
+        
     };
 
 }
